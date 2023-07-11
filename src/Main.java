b8f2790818b6fb23f2d0ea0f749fd031269cfc2b
jpmedinagl <jpmedinagl@gmail.com>
2023-07-11 17:39:29 -0400

    adding main

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        int rows = 20;
+        int columns = 20;
+
+        Maze maze = new Maze(rows, columns);
+        Generator generator = new Generator(maze);
+        generator.generateMaze();
+        maze.printMaze();
+    }
+}
