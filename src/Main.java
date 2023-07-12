1597ae1dbdc406f02894becbe486928114d0f03f
jpmedinagl <jpmedinagl@gmail.com>
2023-07-11 21:39:38 -0400

    changing files

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
@@ -7,6 +9,7 @@ public class Main {
 
         Maze maze = new Maze(rows, columns);
         maze.generateMaze();
+
         maze.printMaze();
     }
 }
--- src/Maze.java
+++ src/Maze.java
@@ -1,14 +1,20 @@
-import java.util.*;
+import java.util.Iterator;
 
-public class Maze {
+public class Maze implements Iterable<int[]> {
     private final int rows;
     private final int columns;
     private final int[][] maze;
+    private final Generator generator;
 
     public Maze(int rows, int columns) {
         this.rows = rows;
         this.columns = columns;
         this.maze = new int[rows][columns];
+        this.generator = new DFSGenerator(this);
+    }
+
+    public void generateMaze() {
+        generator.generateMaze();
     }
 
     public int getRows() {
@@ -19,6 +25,10 @@ public class Maze {
         return columns;
     }
 
+    public int[] getRow(int row) {
+        return maze[row];
+    }
+
     public int getCell(int row, int column) {
         return maze[row][column];
     }
@@ -27,83 +37,14 @@ public class Maze {
         maze[row][column] = value;
     }
 
-    public void generateMaze() {
-        Random random = new Random();
-
-        // Initialize maze with walls
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < columns; j++) {
-                maze[i][j] = 1; // 1 represents walls
-            }
-        }
-
-        // int startRow = random.nextInt(rows);
-        // int startColumn = random.nextInt(columns);
-
-        int startRow = 1;
-        int startColumn = 0;
-
-        generateMazeRecursive(startRow, startColumn);
-
-        // Set start and end points
-        maze[startRow][startColumn] = 2; // 2 represents the start point
-        maze[rows - 1][columns - 1] = 3; // 3 represents the end point
-    }
-
-    private void generateMazeRecursive(int row, int col) {
-        int[] directions = { 1, 2, 3, 4 }; // 1: up, 2: right, 3: down, 4: left
-        shuffleArray(directions);
-
-        for (int direction : directions) {
-            int newRow = row;
-            int newCol = col;
-
-            if (direction == 1) {
-                newRow -= 2;
-            } else if (direction == 2) {
-                newCol += 2;
-            } else if (direction == 3) {
-                newRow += 2;
-            } else if (direction == 4) {
-                newCol -= 2;
-            }
-
-            if (newRow >= 0 && newRow < rows && newCol >= 0 && newCol < columns && maze[newRow][newCol] == 1) {
-                maze[newRow][newCol] = 0; // 0 represents the path
-
-                if (direction == 1) {
-                    maze[row - 1][col] = 0;
-                } else if (direction == 2) {
-                    maze[row][col + 1] = 0;
-                } else if (direction == 3) {
-                    maze[row + 1][col] = 0;
-                } else if (direction == 4) {
-                    maze[row][col - 1] = 0;
-                }
-
-                generateMazeRecursive(newRow, newCol);
-            }
-        }
-    }
-
-    private void shuffleArray(int[] array) {
-        Random random = new Random();
-        for (int i = array.length - 1; i > 0; i--) {
-            int index = random.nextInt(i + 1);
-            int temp = array[index];
-            array[index] = array[i];
-            array[i] = temp;
-        }
-    }
-
     public void printMaze() {
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < columns; j++) {
-                if (maze[i][j] == 1) {
+        for (int[] row : maze) {
+            for (int cell : row) {
+                if (cell == 1) {
                     System.out.print("# "); // Wall
-                } else if (maze[i][j] == 2) {
+                } else if (cell == 2) {
                     System.out.print("S "); // Start
-                } else if (maze[i][j] == 3) {
+                } else if (cell == 3) {
                     System.out.print("E "); // End
                 } else {
                     System.out.print("  "); // Path
@@ -111,5 +52,10 @@ public class Maze {
             }
             System.out.println();
         }
+    }
+
+    @Override
+    public Iterator<int[]> iterator() {
+        return new MazeIterator(this);
     }
 }
